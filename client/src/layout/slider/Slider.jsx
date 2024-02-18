544a8c99326b649b25e3ea6646116ce24bb61dd1
ccxtncntus <ccxtncn00@gmail.com>
2024-02-18 10:21:03 +0700

    update slide

--- client/src/layout/slider/Slider.jsx
+++ client/src/layout/slider/Slider.jsx
@@ -1,14 +1,27 @@
 import "./slider.css";
 import SliderImgs from "./SliderImgs";
 import ListMore from "./ListMore";
+import { useEffect, useState } from "react";
+import * as SliderService from "../../services/SliderService";
+
 const Slider = () => {
+  const [List, setList] = useState([]);
+  const [ListMores, setListMores] = useState([]);
+  useEffect(() => {
+    const run = async () => {
+      const listSlider = await SliderService.list();
+      setListMores(listSlider.slice(-2));
+      setList(listSlider.slice(0, -2));
+    };
+    run();
+  }, []);
   return (
     <div className="silder">
       <div className="col-md-8">
-        <SliderImgs />
+        <SliderImgs List={List} />
       </div>
       <div className="col-md-4">
-        <ListMore />
+        <ListMore ListMores={ListMores} />
       </div>
     </div>
   );
--- client/src/layout/slider/SliderImgs.jsx
+++ client/src/layout/slider/SliderImgs.jsx
@@ -2,38 +2,24 @@ import Carousel from "react-bootstrap/Carousel";
 import Image from "react-bootstrap/Image";
 import "./sliderimg.css";
 import { NavLink } from "react-router-dom";
-const SliderImgs = () => {
-  const data = [
-    {
-      img: "https://statics.pancake.vn/web-media/b4/bc/55/d5/20a66562c81b15093b28d201b184df701f68060300b72c061deab517.jpg",
-      title: "vip nhất",
-      content: "siêu nhân gao biến hình",
-      path: "shop",
-    },
-    {
-      img: "https://statics.pancake.vn/web-media/5f/5c/63/be/b3d04437e541a3ea74033ef1a731fc851749d896110e3efeb6e9f8d2.jpg",
-      title: "vip nhì",
-      content: "Bạn là ai",
-    },
-    {
-      img: "https://statics.pancake.vn/web-media/b1/52/fa/4f/ee7fbe82b60d5bcd8d955c594477fdc74d358be8b315a6b5e57245de.jpg",
-      title: "vip ba",
-      content: "nothing",
-    },
-    {
-      img: "https://statics.pancake.vn/web-media/b1/52/fa/4f/ee7fbe82b60d5bcd8d955c594477fdc74d358be8b315a6b5e57245de.jpg",
-      title: "vip ba",
-      content: "nothing",
-    },
-  ];
+import { useEffect, useState } from "react";
+import { HOST } from "../../configs/DataEnv";
+const SliderImgs = (props) => {
+  const { List } = props;
+  const [Lists, setLists] = useState([]);
+  useEffect(() => {
+    if (List.length > 0) {
+      setLists(List);
+    }
+  }, [List]);
   return (
     <div className="sliderImgs">
       <Carousel fade>
-        {data &&
-          data.map((item, index) => {
+        {Lists &&
+          Lists.map((item, index) => {
             return (
               <Carousel.Item key={index}>
-                <Image src={item.img} thumbnail />
+                <Image src={HOST + "/uploads/" + item.img} thumbnail />
                 <Carousel.Caption>
                   <div className="sliderImgs_title">
                     <NavLink to={item.path}>{item.title}</NavLink>
